97745de4e0fe4f22f417d0b42d2f22eee228f651
Tim Roper <tim.roper@myob.com>
2018-09-22 08:47:13 +1000

    Stubbing dependency on TaxTable

--- test/payslip_calculator_test.exs
+++ test/payslip_calculator_test.exs
@@ -1,12 +1,30 @@
 defmodule PayslipCalculatorTest do
   use ExUnit.Case
 
+  import Mock
+
   test "Calculates the payslip" do
-    assert PayslipCalculator.monthly_pay(60_050, 0.09) == 4082
-    assert PayslipCalculator.gross_income(60_050) == 5004
-    assert PayslipCalculator.income_tax(3572, 60_050, 37_000, 0.325) == 922
-    assert PayslipCalculator.net_income(5004, 922) == 4082
-    assert PayslipCalculator.super(5004, 0.09) == 450
+    with_mock TaxTable, [lookup: fn(_) -> {3572, 37_000, 0.325} end] do 
+      assert PayslipCalculator.monthly_pay(60_050, 0.09) == 4082
+    end
+  end
+    
+  test "Calculates the net income" do
+    with_mock TaxTable, [lookup: fn(_) -> {3572, 37_000, 0.325} end] do 
+      assert PayslipCalculator.net_income(5004, 922) == 4082
+    end
+  end
+
+  test "Calculates the superannuation" do
+    with_mock TaxTable, [lookup: fn(_) -> {3572, 37_000, 0.325} end] do 
+      assert PayslipCalculator.super(5004, 0.09) == 450
+    end
+  end
+
+  test "Calculates the income tax" do
+    with_mock TaxTable, [lookup: fn(_) -> {3572, 37_000, 0.325} end] do 
+      assert PayslipCalculator.income_tax(3572, 60_050, 37_000, 0.325) == 922
+    end
   end
 
 end
